a186a1ca3280e74ec19d70668970c345e75b94ec
jsswpt <paiusov.d@yandex.ru>
2025-03-04 14:05:24 +0500

    hotfix: add prop

--- src/screens/pages/profile/ui/fallback.tsx
+++ src/screens/pages/profile/ui/fallback.tsx
@@ -1,5 +1,5 @@
 import { Template } from './template'
 
 export const Fallback = () => (
-  <Template userData={<>// Тут можно сделать скелетон</>} />
+  <Template userData={<>// Тут можно сделать скелетон</>} quote={<>// Тут можно сделать скелетон</>}/>
 )
